cc50d5c99e54239fe8e7915859d85c1367d84aae
SumitKumar1307 <76728304+SumitKumar1307@users.noreply.github.com>
2021-11-07 15:07:22 +0530

    Added the Register section

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_remake/ui/register.dart';
+import 'package:todo_remake/ui/registration.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/ui/registration.dart
+++ lib/ui/registration.dart
@@ -110,6 +110,96 @@ class _RegistrationState extends State<Registration> {
                     ],
                   ),
                 ),
+                SizedBox(height: 45),
+                Text(
+                  "Register In",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 40),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: 70,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.only(left: 10, right: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(19)),
+                  color: Color(0xffF4F9FC),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        "Register With Email",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(
+                        Icons.email_outlined,
+                        color: Colors.black,
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: 70,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.only(left: 10, right: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(19)),
+                  color: Color(0xffF4F9FC),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        "Register With Microsoft",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(
+                        FontAwesomeIcons.microsoft,
+                        color: Colors.black,
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: 70,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.only(left: 10, right: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(19)),
+                  color: Color(0xffF4F9FC),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        "Register With Google",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(
+                        FontAwesomeIcons.google,
+                        color: Colors.black,
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 75,)
               ],
             ),
           ),
